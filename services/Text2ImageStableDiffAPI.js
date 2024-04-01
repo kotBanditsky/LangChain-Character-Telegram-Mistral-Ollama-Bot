3775ffc565930a078e7e757b4e8f063441677c36
kotBanditsky <arhydro@protonmail.com>
2024-04-01 03:28:27 +0300

    Update Text2ImageStableDiffAPI.js

--- services/Text2ImageStableDiffAPI.js
+++ services/Text2ImageStableDiffAPI.js
@@ -1,48 +1,11 @@
 import axios from "axios";
 import fs from "fs";
+import config from "../config";
 
 async function generateImage(response) {
   const request = await axios.post("http://127.0.0.1:7860/sdapi/v1/txt2img", {
     prompt: response,
-    negative_prompt: "",
-    seed: -1,
-    subseed: -1,
-    subseed_strength: 0,
-    seed_resize_from_h: -1,
-    seed_resize_from_w: -1,
-    sampler_name: "Euler a",
-    batch_size: 1,
-    n_iter: 1,
-    steps: 20,
-    cfg_scale: 7,
-    width: 512,
-    height: 512,
-    restore_faces: true,
-    tiling: true,
-    do_not_save_samples: false,
-    do_not_save_grid: false,
-    eta: 0,
-    denoising_strength: 0,
-    s_min_uncond: 0,
-    s_churn: 0,
-    s_tmax: 0,
-    s_tmin: 0,
-    s_noise: 0,
-    override_settings: {},
-    override_settings_restore_afterwards: true,
-    refiner_switch_at: 0,
-    disable_extra_networks: false,
-    enable_hr: false,
-    firstphase_width: 0,
-    firstphase_height: 0,
-    hr_scale: 2,
-    hr_second_pass_steps: 0,
-    hr_resize_x: 0,
-    hr_resize_y: 0,
-    hr_prompt: "",
-    hr_negative_prompt: "",
-    send_images: true,
-    save_images: false,
+    negative_prompt: config.NEGATIVE_PROMT,
   });
 
   let images = await request.data.images;
